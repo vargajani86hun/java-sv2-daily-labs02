9b867e06bca4499c5aee776fe258db496f285286
JaniVarga86HUN <jani.varga86@gmail.com>
2021-10-20 22:19:30 +0200

    Project created, Human class is ready.

---
+++ week02-labs-tasks/src/main/java/day01/Human.java
@@ -0,0 +1,19 @@
+package day01;
+
+public class Human {
+    private String name;
+    private int age;
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
